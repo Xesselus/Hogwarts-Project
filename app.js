4fc1a2dd5233a3f859b8a9cc870d3a1a0789a22b
Bogdan Olteanu <boboolte@yahoo.com>
2022-01-16 12:36:01 +0200

    resolved task

---
+++ app.js
@@ -0,0 +1,247 @@
+let grifLore = document.getElementById("grif-lore");
+grifLore.style.display = "none";
+
+let dumbledore = document.getElementById("dumbledore");
+dumbledore.style.display = "none";
+
+let dumbleLore = document.getElementById("dumble-lore");
+dumbleLore.style.display = "none";
+
+let harry = document.getElementById("harry");
+harry.style.display = "none";
+
+let harryLore = document.getElementById("harry-lore");
+harryLore.style.display = "none";
+
+let grifLogo = document.getElementById("grif");
+
+grifLogo.addEventListener("click", () => {
+  grifLogo.style.marginLeft = "200px";
+  grifLore.style.display = "inline";
+  huffleLogo.style.marginLeft = "10px";
+  huffleLore.style.display = "none";
+  ravenLogo.style.marginLeft = "10px";
+  ravenLore.style.display = "none";
+  slithLogo.style.marginLeft = "10px";
+  slithLore.style.display = "none";
+  dumbledore.style.display = "inline";
+  dumbledore.style.marginLeft = "870px";
+  dumbledore.style.marginTop = "-370px";
+  dumbleLore.style.display = "none";
+  harryLore.style.display = "none";
+  harry.style.display = "inline";
+  harry.style.marginLeft = "870px";
+  harry.style.marginTop = "5px";
+  olivLore.style.display = "none";
+  olivander.style.display = "none";
+  lokHart.style.display = "none";
+  lokLore.style.display = "none";
+  tonksLore.style.display = "none";
+  tonks.style.display = "none";
+  newt.style.display = "none";
+  newtLore.style.display = "none";
+  volLore.style.display = "none";
+  voldemort.style.display = "none";
+  snape.style.display = "none";
+  snapeLore.style.display = "none";
+
+  dumbledore.addEventListener("click", () => {
+    dumbleLore.style.display = "inline";
+    dumbleLore.style.marginLeft = "1050px";
+    dumbleLore.style.marginTop = "-100px";
+    dumbleLore.style.width = "400px";
+    harryLore.style.display = "none";
+    olivLore.style.display = "none";
+  });
+  harry.addEventListener("click", () => {
+    harryLore.style.display = "inline";
+    harryLore.style.marginLeft = "1050px";
+    harryLore.style.marginTop = "-100px";
+    harryLore.style.width = "400px";
+    dumbleLore.style.display = "none";
+    olivLore.style.display = "none";
+  });
+});
+let ravenLore = document.getElementById("raven-lore");
+ravenLore.style.display = "none";
+
+let olivander = document.getElementById("olivander");
+olivander.style.display = "none";
+
+let olivLore = document.getElementById("olivander-lore");
+olivLore.style.display = "none";
+
+let lokHart = document.getElementById("lockhart");
+lokHart.style.display = "none";
+
+let lokLore = document.getElementById("lock-lore");
+lokLore.style.display = "none";
+
+let ravenLogo = document.getElementById("raven");
+ravenLogo.addEventListener("click", () => {
+  grifLogo.style.marginLeft = "10px";
+  grifLore.style.display = "none";
+  ravenLogo.style.marginLeft = "200px";
+  ravenLore.style.display = "inline";
+  huffleLogo.style.marginLeft = "10px";
+  huffleLore.style.display = "none";
+  slithLogo.style.marginLeft = "10px";
+  slithLore.style.display = "none";
+  harry.style.display = "none";
+  harryLore.style.display = "none";
+  dumbledore.style.display = "none";
+  dumbleLore.style.display = "none";
+  olivander.style.display = "inline";
+  olivander.style.display = "inline";
+  olivander.style.marginLeft = "870px";
+  olivander.style.marginTop = "-370px";
+  olivLore.style.display = "none";
+  lokLore.style.display = "none";
+  lokHart.style.display = "inline";
+  lokHart.style.marginLeft = "870px";
+  lokHart.style.marginTop = "5px";
+  tonksLore.style.display = "none";
+  tonks.style.display = "none";
+  newt.style.display = "none";
+  newtLore.style.display = "none";
+  volLore.style.display = "none";
+  voldemort.style.display = "none";
+  snape.style.display = "none";
+  snapeLore.style.display = "none";
+
+  olivander.addEventListener("click", () => {
+    olivLore.style.display = "inline";
+    olivLore.style.marginLeft = "1050px";
+    olivLore.style.marginTop = "-100px";
+    olivLore.style.width = "400px";
+    lokLore.style.display = "none";
+  });
+  lokHart.addEventListener("click", () => {
+    lokLore.style.display = "inline";
+    lokLore.style.marginLeft = "1050px";
+    lokLore.style.marginTop = "-100px";
+    lokLore.style.width = "400px";
+    olivLore.style.display = "none";
+  });
+});
+
+let huffleLore = document.getElementById("huffle-lore");
+huffleLore.style.display = "none";
+
+let tonks = document.getElementById("tonks");
+tonks.style.display = "none";
+
+let tonksLore = document.getElementById("tonks-lore");
+tonksLore.style.display = "none";
+
+let newt = document.getElementById("newt");
+newt.style.display = "none";
+
+let newtLore = document.getElementById("newt-lore");
+newtLore.style.display = "none";
+
+let huffleLogo = document.getElementById("huffle");
+huffleLogo.addEventListener("click", () => {
+  grifLogo.style.marginLeft = "10px";
+  grifLore.style.display = "none";
+  ravenLogo.style.marginLeft = "10px";
+  ravenLore.style.display = "none";
+  huffleLogo.style.marginLeft = "200px";
+  huffleLore.style.display = "inline";
+  slithLogo.style.marginLeft = "10px";
+  slithLore.style.display = "none";
+  harry.style.display = "none";
+  dumbledore.style.display = "none";
+  lokHart.style.display = "none";
+  lokLore.style.display = "none";
+  olivLore.style.display = "none";
+  olivander.style.display = "none";
+  tonksLore.style.display = "none";
+  tonks.style.display = "inline";
+  tonks.style.marginLeft = "870px";
+  tonks.style.marginTop = "-450px";
+  newtLore.style.display = "none";
+  newt.style.display = "inline";
+  newt.style.marginLeft = "870px";
+  newt.style.marginTop = "5px";
+  volLore.style.display = "none";
+  voldemort.style.display = "none";
+  snape.style.display = "none";
+  snapeLore.style.display = "none";
+  tonks.addEventListener("click", () => {
+    tonksLore.style.display = "inline";
+    tonksLore.style.marginLeft = "1050px";
+    tonksLore.style.marginTop = "-100px";
+    tonksLore.style.width = "400px";
+    newtLore.style.display = "none";
+  });
+  newt.addEventListener("click", () => {
+    newtLore.style.display = "inline";
+    newtLore.style.marginLeft = "1050px";
+    newtLore.style.marginTop = "-100px";
+    newtLore.style.width = "400px";
+    tonksLore.style.display = "none";
+  });
+});
+let slithLore = document.getElementById("slith-lore");
+slithLore.style.display = "none";
+
+let voldemort = document.getElementById("voldemort");
+voldemort.style.display = "none";
+
+let volLore = document.getElementById("voldemort-lore");
+volLore.style.display = "none";
+
+let snape = document.getElementById("snape");
+snape.style.display = "none";
+
+let snapeLore = document.getElementById("snape-lore");
+snapeLore.style.display = "none";
+
+let slithLogo = document.getElementById("slith");
+slithLogo.addEventListener("click", () => {
+  grifLogo.style.marginLeft = "10px";
+  grifLore.style.display = "none";
+  ravenLogo.style.marginLeft = "10px";
+  ravenLore.style.display = "none";
+  huffleLogo.style.marginLeft = "10px";
+  huffleLore.style.display = "none";
+  slithLogo.style.marginLeft = "200px";
+  slithLore.style.display = "inline";
+  slithLore.style.width = "700px";
+  volLore.style.display = "none";
+  voldemort.style.display = "inline";
+  voldemort.style.marginLeft = "1170px";
+  voldemort.style.marginTop = "-500px";
+  harryLore.style.display = "none";
+  harry.style.display = "none";
+  dumbleLore.style.display = "none";
+  dumbledore.style.display = "none";
+  newt.style.display = "none";
+  newtLore.style.display = "none";
+  tonks.style.display = "none";
+  tonksLore.style.display = "none";
+  olivLore.style.display = "none";
+  olivander.style.display = "none";
+  lokLore.style.display = "none";
+  lokHart.style.display = "none";
+  snapeLore.style.display = "none";
+  snape.style.display = "inline";
+  snape.style.marginLeft = "1170px";
+  snape.style.marginTop = "5px";
+
+  voldemort.addEventListener("click", () => {
+    volLore.style.display = "inline";
+    volLore.style.marginLeft = "1350px";
+    volLore.style.marginTop = "-100px";
+    volLore.style.width = "400px";
+    snapeLore.style.display = "none";
+  });
+  snape.addEventListener("click", () => {
+    snapeLore.style.display = "inline";
+    snapeLore.style.marginLeft = "1350px";
+    snapeLore.style.marginTop = "-100px";
+    snapeLore.style.width = "400px";
+    volLore.style.display = "none";
+  });
+});
